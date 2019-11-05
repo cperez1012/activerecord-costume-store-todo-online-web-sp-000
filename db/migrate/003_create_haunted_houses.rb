07857f57778a2af1f584f7f7b4f1e4aaefb987cb
Christian Perez <perezchristian1012@gmail.com>
2019-11-05 02:02:23 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,13 @@
 class HauntedHouse < ActiveRecord:Migration[5.2]
-
+  def change
+    create_table :haunted_houses do |h|
+      h.string  :name
+      h.string  :location
+      h.string  :theme
+      h.integer :price
+      h.boolean :family_friend_or_not
+      h.datetime  :opening_date
+      h.datetime  :closing_date
+      h.string  :long_description
+  end
 end
