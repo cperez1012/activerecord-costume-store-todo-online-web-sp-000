cd7220dbe031840a19966a501a2b566a0aa65a98
Christian Perez <perezchristian1012@gmail.com>
2019-11-05 01:46:44 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -3,7 +3,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
   def change
     create_table :costumes do |c|
       c.string :name
-      c.float :price
+      c.integer :price
       c.string :image_url
       c.string :size
       c.datetime :created_at
