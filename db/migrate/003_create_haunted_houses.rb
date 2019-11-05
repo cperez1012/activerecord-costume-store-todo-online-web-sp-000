120ac8f4fed1136d4d8d1a23a1a3172077c79144
Christian Perez <perezchristian1012@gmail.com>
2019-11-05 02:24:08 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,4 +1,4 @@
-class CreateHauntedHouse < ActiveRecord::Migration[5.2]
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
   def change
     create_table :haunted_houses do |h|
       h.string  :name
