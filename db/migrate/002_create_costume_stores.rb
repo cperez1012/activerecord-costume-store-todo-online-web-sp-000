1a43fe037b3f4add47b5326374f73c004bcd30ba
Christian Perez <perezchristian1012@gmail.com>
2019-11-05 02:18:37 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,4 +1,4 @@
-class CostumeStore < ActiveRecord:Migration[5.2]
+class CreateCostumeStore < ActiveRecord::Migration[5.2]
   def change
     create_table :costume_stores do |c|
       c.string :name
